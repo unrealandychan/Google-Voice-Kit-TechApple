d8749c6257e7e88481bfd6c9f81d6c0360fc8d50
unrealandychan <unrealandychan@gmail.com>
2018-07-19 17:57:43 +0800

    Voice kit is ready

---
+++ voice/Setup.py
@@ -0,0 +1,12 @@
+import subprocess
+
+
+
+subprocess.call('sudo mv my_assistant_one.service /lib/systemd/system/',shell = True)
+subprocess.call('sudo mv my_assistant_two.service /lib/systemd/system/',shell = True)
+subprocess.call('sudo systemctl enable my_assistant_one.service',shell = True)
+subprocess.call('sudo systemctl enable my_assistant_two.service',shell = True)
+subprocess.call('sudo service my_assistant_one start',shell=True)
+subprocess.call('sudo service my_assistant_two start',shell=True)
+subprocess.call('sudo service my_assistant_two stop',shell=True)
+
---
+++ voice/test.py
@@ -0,0 +1,4 @@
+import aiy.audio
+import os
+
+aiy.audio.play_wave(os.path.join('mode.wav'))
