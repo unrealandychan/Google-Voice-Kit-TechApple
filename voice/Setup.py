bbf81b3eb6c1c54e8bb406d77b10d3732414302e
unrealandychan <unrealandychan@gmail.com>
2018-07-27 11:45:38 +0800

    update Setup.py

--- voice/Setup.py
+++ voice/Setup.py
@@ -3,10 +3,5 @@ import subprocess
 
 
 subprocess.call('sudo mv my_assistant_one.service /lib/systemd/system/',shell = True)
-subprocess.call('sudo mv my_assistant_two.service /lib/systemd/system/',shell = True)
 subprocess.call('sudo systemctl enable my_assistant_one.service',shell = True)
-subprocess.call('sudo systemctl enable my_assistant_two.service',shell = True)
 subprocess.call('sudo service my_assistant_one start',shell=True)
-subprocess.call('sudo service my_assistant_two start',shell=True)
-subprocess.call('sudo service my_assistant_two stop',shell=True)
-
